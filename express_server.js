e65ced203bb3e5e01c8e9e2e3e8af1578d03e68d
Joseph McGregor <joseph00@my.yorku.ca>
2021-12-09 13:34:42 -0500

    header working + linted file

--- express_server.js
+++ express_server.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8081;
 
-// app.set("view engine", "ejs");
+app.set("view engine", "ejs");
 
-// const urlDatabase = {
-//   "b2xVn2": "http://www.lighthouselabs.ca",
-//   "9sm5xK": "http://www.google.com"
-// };
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -21,9 +21,9 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
@@ -31,6 +31,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: /* What goes here? */ };
+const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[ req.params.shortURL ] };
   res.render("urls_show", templateVars);
 });
+
