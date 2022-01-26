4e9d95ec81bd67b3b957585802d0320f4f010847
Joseph McGregor <joseph00@my.yorku.ca>
2022-01-25 19:07:15 -0500

    added username path with cookies

--- express_server.js
+++ express_server.js
@@ -52,16 +52,21 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const username = req.cookies["username"]
+  const templateVars = { username, urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const username = req.cookies["username"]
+  const templateVars = { username };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  const username = req.cookies["username"]
+  const templateVars = { username, shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+
   res.render("urls_show", templateVars);
 });
 
@@ -85,6 +90,13 @@ app.post("/urls/:id", (req, res) => {
   const shortURL = req.params.id
   const longURL = req.body.longURL
   urlDatabase[shortURL] = longURL
+  res.redirect("/urls")
+});
+
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie("username", username)
   res.redirect("/urls")
 });
 
